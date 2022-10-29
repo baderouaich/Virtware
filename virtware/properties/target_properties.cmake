f459ae82ef1942b2fdba8c6a4d173b6844c03afc
Bader Ouaich <ouaichbadereddine@gmail.com>
2022-10-29 12:26:31 +0100

    re-organizing project structure

--- virtware/properties/target_properties.cmake
+++
@@ -1,4 +0,0 @@
-target_default_namespace("virtware")
-target_icon(resources/virtware)
-target_name("virtware")
-target_startup("virtware::form1" src/form1.h)
--- virtware_script/properties/target_properties.cmake
+++
@@ -1,3 +0,0 @@
-target_default_namespace("vhelang")
-target_name("vhelang")
-target_startup("vhelang::program" src/program.h)
