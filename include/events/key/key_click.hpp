1ca27a43bd05ba996507ffa12ca8c19f427a2874
Bader Ouaich <ouaichbadereddine@gmail.com>
2022-11-04 23:42:20 +0100

    added key click event

---
+++ include/events/key/key_click.hpp
@@ -0,0 +1,34 @@
+#pragma once
+#include "../event.hpp"
+#include <cstdint>
+
+namespace virtware
+{
+    class KeyClickEvent : public Event
+    {
+    public:
+        explicit KeyClickEvent(const std::uint16_t keycode);
+
+        std::uint16_t get_keycode() const noexcept;
+
+    public:
+        /**
+         * @brief Return current event as a string representation
+         * @example: key click tab
+         *           key click caps_lock
+         *           key click a
+         * @return std::string
+         */
+        std::string to_string() const override;
+
+        /**
+         * @brief Instantiates current event object from string representation
+         * @example: str = "key click caps_lock" will initialize KeyClickEvent::m_keycode with caps_lock's keycode
+         * @param str
+         */
+        void from_string(const std::string& str) override;
+
+    private:
+        std::uint16_t m_keycode;
+    };
+}
