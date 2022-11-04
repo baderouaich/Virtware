04b99c932fcaa8b97cca4149c85420d7f42a90da
Bader Ouaich <ouaichbadereddine@gmail.com>
2022-11-04 23:44:04 +0100

    added key events handling in executor

--- src/executor/executor.cpp
+++ src/executor/executor.cpp
@@ -5,6 +5,10 @@
 #include "events/mouse/mouse_press.hpp"
 #include "events/mouse/mouse_release.hpp"
 #include "events/mouse/mouse_scroll.hpp"
+#include "events/key/key_press.hpp"
+#include "events/key/key_release.hpp"
+#include "events/key/key_click.hpp"
+#include "io/key/key.hpp"
 #include <utility>
 #include <stdexcept>
 using namespace virtware;
@@ -22,12 +26,24 @@ void Executor::execute(const std::shared_ptr<Event>& event)
 	using enum Event::EventType;
 	switch (event->get_type())
 	{
-	case Key: {
-		
+	case Key: 
+	{
+		if (KeyPressEvent* key_press_event = dynamic_cast<KeyPressEvent*>(event.get()))
+		{
+			Key::press(key_press_event->get_keycode());
+		}
+		else if (KeyReleaseEvent* key_release_event = dynamic_cast<KeyReleasesEvent*>(event.get()))
+		{
+			Key::release(key_release_event->get_keycode());
+		}
+		else if (KeyClickEvent* key_click_event = dynamic_cast<KeyClickEvent*>(event.get()))
+		{
+			Key::click(key_click_event->get_keycode());
+		}
 		break;
 	}
 	case Mouse: {
-		if(MouseMoveEvent* mouse_move = dynamic_cast<MouseMoveEvent*>(event.get()))
+		if(MouseMoveEvent* mouse_move_event = dynamic_cast<MouseMoveEvent*>(event.get()))
 		{
 
 		}
