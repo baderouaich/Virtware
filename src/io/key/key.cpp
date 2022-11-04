19f96080520323e421d1d536247f487370acc5a8
Bader Ouaich <ouaichbadereddine@gmail.com>
2022-11-04 22:37:11 +0100

    added key io performer

--- src/io/key/key.cpp
+++ src/io/key/key.cpp
@@ -1,1 +1,27 @@
 #include "pch.hpp"
+#include "io/key/key.hpp"
+using namespace virtware;
+
+void Key::press(const std::uint16_t keycode) noexcept
+{
+    ::INPUT input{ 0 };
+    input.type = INPUT_KEYBOARD;
+    input.ki.wVk = static_cast<decltype(input.ki.wVk)>(keycode);
+    input.ki.dwFlags = 0;
+    ::SendInput(1, &input, sizeof(::INPUT));
+}
+
+void Key::release(const std::uint16_t keycode) noexcept
+{
+    ::INPUT input{ 0 };
+    input.type = INPUT_KEYBOARD;
+    input.ki.wVk = static_cast<decltype(input.ki.wVk)>(keycode);
+    input.ki.dwFlags = KEYEVENTF_KEYUP;
+    ::SendInput(1, &input, sizeof(::INPUT));
+}
+
+void Key::click(const std::uint16_t keycode) noexcept
+{
+    press(keycode);
+    release(keycode);
+}
