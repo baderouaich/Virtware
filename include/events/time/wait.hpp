35d8259738014a5061240d2a7a9611d91e1f9287
Bader Ouaich <ouaichbadereddine@gmail.com>
2022-11-04 20:55:27 +0100

    added wait event

---
+++ include/events/time/wait.hpp
@@ -0,0 +1,59 @@
+#pragma once
+#include "events/event.hpp"
+#include <chrono>
+#include <string>
+#include <sstream>
+
+namespace virtware
+{
+	class WaitEvent : public Event
+	{
+    public:
+        //static inline const std::string NANOSECONDS_PREFIX = "nc";
+        static inline const std::string MICROSECONDS_PREFIX = "mc";
+        static inline const std::string MILLISECONDS_PREFIX = "ms";
+        static inline const std::string SECONDS_PREFIX = "s";
+        static inline const std::string MINUTES_PREFIX = "min";
+        static inline const std::string HOURS_PREFIX = "hr";
+        static inline const std::string DAYS_PREFIX = "d";
+        static inline const std::string WEEKS_PREFIX = "w";
+        static inline const std::string MONTHS_PREFIX = "mon";
+        static inline const std::string YEARS_PREFIX = "yr";
+
+	public:
+		WaitEvent();
+		WaitEvent(const std::chrono::system_clock::duration& duration, const std::string& suffix);
+		WaitEvent(const std::size_t units, const std::string& suffix);
+        //explicit WaitEvent(const std::chrono::nanoseconds& duration);
+        explicit WaitEvent(const std::chrono::microseconds& duration);
+		explicit WaitEvent(const std::chrono::milliseconds& duration);
+		explicit WaitEvent(const std::chrono::seconds& duration);
+		explicit WaitEvent(const std::chrono::minutes& duration);
+		explicit WaitEvent(const std::chrono::hours& duration);
+		explicit WaitEvent(const std::chrono::days& duration);
+		explicit WaitEvent(const std::chrono::weeks& duration);
+		explicit WaitEvent(const std::chrono::months& duration);
+		explicit WaitEvent(const std::chrono::years& duration);
+
+
+        /**
+         * @brief Return current event as a string representation.
+         * @example: wait 100ms
+         *           wait 2s
+         *
+         * @return std::string
+         */
+        std::string to_string() const override;
+
+        /**
+         * @brief Instantiates current event object from string representation.
+         * @example: str = "wait 300ms" will initialize WaitEvent::duration to 300ms
+         * @param str
+         */
+        void from_string(const std::string& str) override;
+
+	private:
+		std::chrono::system_clock::duration m_duration; /**< Duration to wait for */
+		std::string m_suffix; /**< Suffix string after duration number, example: 300ms, 1s, 10hr ... */
+	};
+}
---
+++ src/events/time/wait.cpp
@@ -0,0 +1,128 @@
+#include "pch.hpp"
+#include "events/time/wait.hpp"
+using namespace virtware;
+
+WaitEvent::WaitEvent() : 
+	Event(EventType::Time)
+{
+}
+
+WaitEvent::WaitEvent(const std::chrono::system_clock::duration& duration, const std::string& suffix)
+	:
+	Event(EventType::Time),
+	m_duration(duration),
+	m_suffix(suffix)
+{
+}
+
+WaitEvent::WaitEvent(const std::size_t units, const std::string& suffix)
+    : Event(EventType::Time)
+{
+    this->from_string("wait " + std::to_string(units) + suffix);
+}
+
+WaitEvent::WaitEvent(const std::chrono::microseconds& duration)
+	: WaitEvent(duration, MICROSECONDS_PREFIX)
+{
+}
+
+WaitEvent::WaitEvent(const std::chrono::milliseconds& duration)
+	: WaitEvent(duration, MILLISECONDS_PREFIX)
+{
+}
+
+WaitEvent::WaitEvent(const std::chrono::seconds& duration)
+	: WaitEvent(duration, SECONDS_PREFIX)
+{
+}
+
+WaitEvent::WaitEvent(const std::chrono::minutes& duration)
+	: WaitEvent(duration, MINUTES_PREFIX)
+{
+}
+
+WaitEvent::WaitEvent(const std::chrono::hours& duration)
+	: WaitEvent(duration, HOURS_PREFIX)
+{
+}
+
+WaitEvent::WaitEvent(const std::chrono::days& duration)
+	: WaitEvent(duration, DAYS_PREFIX)
+{
+}
+
+WaitEvent::WaitEvent(const std::chrono::weeks& duration)
+	: WaitEvent(duration, WEEKS_PREFIX)
+{
+}
+
+WaitEvent::WaitEvent(const std::chrono::months& duration)
+    : WaitEvent(duration, MONTHS_PREFIX)
+{
+}
+
+WaitEvent::WaitEvent(const std::chrono::years& duration)
+    : WaitEvent(duration, YEARS_PREFIX)
+{
+}
+
+
+std::string WaitEvent::to_string() const
+{
+    std::ostringstream oss{};
+    oss << "wait ";
+    if (m_suffix == MICROSECONDS_PREFIX)
+        oss << std::chrono::duration_cast<std::chrono::microseconds>(m_duration).count();
+    else if (m_suffix == MILLISECONDS_PREFIX)
+        oss << std::chrono::duration_cast<std::chrono::milliseconds>(m_duration).count();
+    else if (m_suffix == SECONDS_PREFIX)
+        oss << std::chrono::duration_cast<std::chrono::seconds>(m_duration).count();
+    else if (m_suffix == MINUTES_PREFIX)
+        oss << std::chrono::duration_cast<std::chrono::minutes>(m_duration).count();
+    else if (m_suffix == HOURS_PREFIX)
+        oss << std::chrono::duration_cast<std::chrono::hours>(m_duration).count();
+    else if (m_suffix == DAYS_PREFIX)
+        oss << std::chrono::duration_cast<std::chrono::days>(m_duration).count();
+    else if (m_suffix == WEEKS_PREFIX)
+        oss << std::chrono::duration_cast<std::chrono::weeks>(m_duration).count();
+    else if (m_suffix == MONTHS_PREFIX)
+        oss << std::chrono::duration_cast<std::chrono::months>(m_duration).count();
+    else if (m_suffix == YEARS_PREFIX)
+        oss << std::chrono::duration_cast<std::chrono::years>(m_duration).count();
+    else
+        throw std::logic_error("Invalid WaitEvent duration suffix '" + m_suffix + '\'');
+    oss << m_suffix;
+    return oss.str();
+}
+
+void WaitEvent::from_string(const std::string& str)
+{
+    // Scan expected format to extract duration count and duration suffix
+    std::size_t units{ 0 };
+    char duration_suffix[20]{ '\000' };
+    if (std::sscanf(str.c_str(), "wait %zu%s", &units, duration_suffix) != 2)
+        throw std::logic_error("Failed to parse WaitEvent from '" + str + '\'');
+    // Assign duration suffix
+    m_suffix = std::string(duration_suffix);
+    // Assign duration count according to  duration suffix
+    if (m_suffix == MICROSECONDS_PREFIX)
+        m_duration = std::chrono::microseconds(units);
+    else if (m_suffix == MILLISECONDS_PREFIX)
+        m_duration = std::chrono::milliseconds(units);
+    else if (m_suffix == SECONDS_PREFIX)
+        m_duration = std::chrono::seconds(units);
+    else if (m_suffix == MINUTES_PREFIX)
+        m_duration = std::chrono::minutes(units);
+    else if (m_suffix == HOURS_PREFIX)
+        m_duration = std::chrono::hours(units);
+    else if (m_suffix == DAYS_PREFIX)
+        m_duration = std::chrono::days(units);
+    else if (m_suffix == WEEKS_PREFIX)
+        m_duration = std::chrono::weeks(units);
+    else if (m_suffix == MONTHS_PREFIX)
+        m_duration = std::chrono::months(units);
+    else if (m_suffix == YEARS_PREFIX)
+        m_duration = std::chrono::years(units);
+    else
+        throw std::logic_error("Failed to parse WaitEvent from '" + str + '\'');
+}
