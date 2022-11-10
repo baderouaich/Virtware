0bd673538aad7d5b03391844e534e2e4f92c5529
Bader Ouaich <ouaichbadereddine@gmail.com>
2022-11-10 17:43:46 +0100

    updated main_form

--- src/gui/main_form.cpp
+++ src/gui/main_form.cpp
@@ -1,16 +1,18 @@
-#include "pch.hpp"
+#include "core/core.hpp"
 #include <gui/main_form.hpp>
 #include "io/mouse/mouse.hpp"
 #include "events/mouse/mouse_move.hpp"
+#include "executor/executor.hpp"
 using namespace xtd::forms;
-using namespace virtware;
+using namespace Virtware;
 
 main_form::main_form() :
 	m_start_recording_btn(new button()),
 	m_stop_recording_btn(new button()), 
 	m_play_recording(new button()),
 	m_events_lst(new list_box()),
-	m_recorder(new Recorder())
+	m_recorder(new Recorder()),
+	m_executor(new Executor())
 {
   text("Virtware");
   client_size({800, 600});
@@ -47,11 +49,18 @@ main_form::main_form() :
 	  .size({ 170,50 })
 	  .font(xtd::drawing::font(xtd::drawing::system_fonts::default_font(), 15.0f))
 	  .click += [this] {
-			for (auto e : m_events_lst->items())
+			for (auto &str : m_events_lst->items())
 			{
-				MouseMoveEvent event(e.to_string());
-				Mouse::set_position({ event.get_x(), event.get_y() });
-				std::this_thread::sleep_for(std::chrono::milliseconds(100));
+				if (str.to_string().starts_with("mouse move"))
+				{
+					m_executor->execute(std::make_shared<MouseMoveEvent>(str.to_string()));
+					std::this_thread::sleep_for(std::chrono::milliseconds(50));
+				}
+				// TODO: How to convert back str to polymorphic Event ?
+			/*	if(str.to_string().contains("mouse"))
+				std::shared_ptr<Event> event(new Mouse);
+				m_executor->execute(event);*/
+				//std::this_thread::sleep_for(std::chrono::milliseconds(100));
 			}
 	  };
   m_events_lst->parent(*this)
