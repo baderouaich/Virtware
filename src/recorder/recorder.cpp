93e4eeb8b9d16dd7d4c427f1e1285ce105213e28
Bader Ouaich <ouaichbadereddine@gmail.com>
2022-11-10 17:37:48 +0100

    divided key and mouse handling code to functions

--- src/recorder/recorder.cpp
+++ src/recorder/recorder.cpp
@@ -1,15 +1,15 @@
-#include "pch.hpp"
+#include "core/core.hpp"
 #include "recorder/recorder.hpp"
 #include "io/key/key.hpp"
 #include "events/mouse/mouse_move.hpp"
 #include "events/time/wait.hpp"
 #include "events/key/key_press.hpp"
-#include "pch.hpp"
+#include "core/core.hpp"
 #include "io/mouse/mouse.hpp"
 #include <xtd/diagnostics/debug.h>
 #include <map>
 #include <string>
-using namespace virtware;
+using namespace Virtware;
 
 Recorder::Recorder()
 : m_is_recording(false) 
@@ -45,45 +45,67 @@ void Recorder::record_loop()
     while(m_is_recording)
     {
         static std::chrono::system_clock::time_point last_event_time = std::chrono::system_clock::now(); // Duration between events, its also an event.
-        std::chrono::system_clock::time_point current_event_time;
+        //std::chrono::system_clock::time_point current_event_time;
         
-        // Check keyboard state 
-        for(const auto& [keycode, keyname] : Key::KEYBOARD)
-        {
-            if(Key::is_key_pressed(keycode))
-            {
-                m_events.push_back(std::make_shared<KeyPressEvent>(keycode));
-                xtd::diagnostics::debug::write_line("Key {} pressed", keyname);
-            }
-            else if(Key::is_key_released(keycode))
-            {
-                xtd::diagnostics::debug::write_line("Key {} released", keyname);
-                /*std::shared_ptr<Event> event = std::make_shared<KeyReleaseEvent>(keycode); 
-                xtd::diagnostics::debug::write_line("Key {} released", keyname);*/
-            }
-        }
+        handle_key_events();
+        handle_mouse_events();
+  
+        /*
         
-        // Check mouse state
-        static vec2i last_mouse_pos{ -1, -1 };
-        vec2i mouse_pos = Mouse::get_position();
-        if (mouse_pos != last_mouse_pos) 
-        {
             current_event_time = std::chrono::system_clock::now();
             
             auto wait_event = current_event_time - last_event_time;
             if(auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(wait_event);
                 ms.count() > 50)
                 m_events.push_back(std::make_shared<WaitEvent>(ms, "ms"));
-            m_events.push_back(std::make_shared<MouseMoveEvent>(mouse_pos));
             last_event_time = current_event_time;
-            last_mouse_pos = mouse_pos;
-        }
 
-        // Check joystick state
-
-
+                */
         std::this_thread::sleep_for(std::chrono::milliseconds(50));
     }
+}
+
+void Recorder::handle_key_events()
+{
+    for(const auto& [keycode, keyname] : Key::KEYBOARD)
+    {
+        if(Key::is_key_pressed(keycode))
+        {
+            m_events.push_back(std::make_shared<KeyPressEvent>(keycode));
+            xtd::diagnostics::debug::write_line("Key {} pressed", keyname);
+        }
+        /*if(Key::is_key_toggled(keycode))
+        {
+            m_events.push_back(std::make_shared<KeyPressEvent>(keycode));
+            xtd::diagnostics::debug::write_line("Key {} toggled: {}", "ON");
+        }*/
+
+    /*    else if(Key::is_key_released(keycode))
+        {
+            xtd::diagnostics::debug::write_line("Key {} released", keyname);
+        }
+        */
+    }  
+}
+
+void Recorder::handle_mouse_events()
+{
+    // Cursor
+    static vec2i last_mouse_pos{ -1, -1 };
+    const vec2i current_mouse_pos = Mouse::get_position();
+    if (current_mouse_pos != last_mouse_pos) 
+    {
+        m_events.push_back(std::make_shared<MouseMoveEvent>(current_mouse_pos));
+        last_mouse_pos = current_mouse_pos;
+    }
+
+    // Buttons
+    
+
+    // Scroll (wheel)
+   /* ::WPARAM wparam;
+    GET_WHEEL_DELTA_WPARAM(wparam);
+    if(wparam.)*/
 }
 
 const std::atomic<bool>& Recorder::is_recording() const noexcept
