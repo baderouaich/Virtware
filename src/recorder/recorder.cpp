01f27f9035b3a91dc78a4b419ff5a9de935cba2a
Bader Ouaich <ouaichbadereddine@gmail.com>
2022-11-11 21:05:28 +0100

    handled wait event in recorder

--- src/recorder/recorder.cpp
+++ src/recorder/recorder.cpp
@@ -41,14 +41,40 @@ void Recorder::stop()
 
 void Recorder::record_loop()
 {
+    static struct Timer
+    {
+        void start()
+        {
+            begin = std::chrono::system_clock::now();
+        }
+
+        std::chrono::milliseconds restart()
+        {
+            const auto now = std::chrono::system_clock::now();
+            const auto diff = std::chrono::duration_cast<std::chrono::milliseconds>(now - begin);
+            start();
+            return diff;
+        }
+
+        std::chrono::system_clock::time_point begin;
+    } timer;
+    timer.start();
+
     m_is_recording = true;
     while(m_is_recording)
     {
         static std::chrono::system_clock::time_point last_event_time = std::chrono::system_clock::now(); // Duration between events, its also an event.
         //std::chrono::system_clock::time_point current_event_time;
         
-        handle_key_events();
-        handle_mouse_events();
+        bool key_event_occured = handle_key_events();
+        bool mouse_event_occured = handle_mouse_events();
+
+        // Time event, duration between two events
+        if (key_event_occured or mouse_event_occured)
+        {
+            auto time = timer.restart();
+            m_events.push_back(std::make_shared<WaitEvent>(time));
+        }
   
         /*
         
@@ -65,12 +91,13 @@ void Recorder::record_loop()
     }
 }
 
-void Recorder::handle_key_events()
+bool Recorder::handle_key_events()
 {
+    bool event_occured = false;
     enum class KeyState : std::uint8_t 
     {
         Dormant, // Key is in idle state, never pressed pressed or released yet
-        Pressed,
+        Pressed, // Key is pressed, used also to detect key released if key state was pressed and not in this frame
     };
     static std::map<std::uint16_t, KeyState> keyboard_state;
     static std::once_flag of;
@@ -91,12 +118,15 @@ void Recorder::handle_key_events()
             {
                 // do nothing
                 xtd::diagnostics::debug::write_line("Key {} was already pressed, ignoring", keyname);
+                event_occured = false;
             }
             else // if (keyboard_state[keycode] == KeyState::Dormant)
             { // otherwise if it was dormant then pressed, its a press event.
                 m_events.push_back(std::make_shared<KeyPressEvent>(keycode));
                 keyboard_state[keycode] = KeyState::Pressed;
                 xtd::diagnostics::debug::write_line("Key {} pressed", keyname);
+                event_occured = true;
+
             }
         }
 #pragma endregion 
@@ -109,14 +139,18 @@ void Recorder::handle_key_events()
                 m_events.push_back(std::make_shared<KeyReleaseEvent>(keycode));
                 keyboard_state[keycode] = KeyState::Dormant;
                 xtd::diagnostics::debug::write_line("Key {} released", keyname);
+                event_occured = true;
             }
         }
 #pragma endregion
     }  
+    return event_occured;
 }
 
-void Recorder::handle_mouse_events()
+bool Recorder::handle_mouse_events()
 {
+    bool event_occured = false;
+
     // Cursor
     static vec2i last_mouse_pos{ -1, -1 };
     const vec2i current_mouse_pos = Mouse::get_position();
@@ -124,6 +158,7 @@ void Recorder::handle_mouse_events()
     {
         m_events.push_back(std::make_shared<MouseMoveEvent>(current_mouse_pos));
         last_mouse_pos = current_mouse_pos;
+        event_occured = true;
     }
 
     // Buttons
@@ -133,6 +168,8 @@ void Recorder::handle_mouse_events()
    /* ::WPARAM wparam;
     GET_WHEEL_DELTA_WPARAM(wparam);
     if(wparam.)*/
+
+    return event_occured;
 }
 
 const std::atomic<bool>& Recorder::is_recording() const noexcept
