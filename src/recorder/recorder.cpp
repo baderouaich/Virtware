f140864a14e053123118511d4ca8ca49499b552b
Bader Ouaich <ouaichbadereddine@gmail.com>
2022-11-11 19:55:12 +0100

    handled key press and release events by recorder

--- src/recorder/recorder.cpp
+++ src/recorder/recorder.cpp
@@ -67,24 +67,51 @@ void Recorder::record_loop()
 
 void Recorder::handle_key_events()
 {
+    enum class KeyState : std::uint8_t 
+    {
+        Dormant, // Key is in idle state, never pressed pressed or released yet
+        Pressed,
+    };
+    static std::map<std::uint16_t, KeyState> keyboard_state;
+    static std::once_flag of;
+    std::call_once(of, [&] 
+    {
+        for (const auto& [keycode, keyname] : Key::KEYBOARD)
+        {
+            keyboard_state[keycode] = KeyState::Dormant;
+        }
+    });
     for(const auto& [keycode, keyname] : Key::KEYBOARD)
     {
+#pragma region handle key pressed event
         if(Key::is_key_pressed(keycode))
         {
-            m_events.push_back(std::make_shared<KeyPressEvent>(keycode));
-            xtd::diagnostics::debug::write_line("Key {} pressed", keyname);
+            // was key already pressed ?
+            if (keyboard_state[keycode] == KeyState::Pressed)
+            {
+                // do nothing
+                xtd::diagnostics::debug::write_line("Key {} was already pressed, ignoring", keyname);
+            }
+            else // if (keyboard_state[keycode] == KeyState::Dormant)
+            { // otherwise if it was dormant then pressed, its a press event.
+                m_events.push_back(std::make_shared<KeyPressEvent>(keycode));
+                keyboard_state[keycode] = KeyState::Pressed;
+                xtd::diagnostics::debug::write_line("Key {} pressed", keyname);
+            }
         }
-        /*if(Key::is_key_toggled(keycode))
+#pragma endregion 
+#pragma region handle key released event
+        else // if it is not pressed now, lets see if it was pressed previously so we can mark it as released
         {
-            m_events.push_back(std::make_shared<KeyPressEvent>(keycode));
-            xtd::diagnostics::debug::write_line("Key {} toggled: {}", "ON");
-        }*/
-
-    /*    else if(Key::is_key_released(keycode))
-        {
-            xtd::diagnostics::debug::write_line("Key {} released", keyname);
+            if (keyboard_state[keycode] == KeyState::Pressed)
+            {
+                // if key was pressed, now its released and we mark it as dormant
+                m_events.push_back(std::make_shared<KeyReleaseEvent>(keycode));
+                keyboard_state[keycode] = KeyState::Dormant;
+                xtd::diagnostics::debug::write_line("Key {} released", keyname);
+            }
         }
-        */
+#pragma endregion
     }  
 }
 
