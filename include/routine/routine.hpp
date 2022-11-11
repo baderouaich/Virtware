4a1a2be905d5fbf3ebb71b81e2286c40f6b2d42f
Bader Ouaich <ouaichbadereddine@gmail.com>
2022-11-11 21:06:13 +0100

    added routine add_event from string

--- include/routine/routine.hpp
+++ include/routine/routine.hpp
@@ -10,14 +10,20 @@ namespace Virtware
 	* @brief A Routine represents a sequence of polymorphic hardware events.
 	* @note A routine can be saved to file, loaded from file, simplified...etc
 	*/
-	class  Routine
+	class Routine
 	{
 	public:
 		Routine();
 		~Routine();
 
 	public:
+		//void from_file(const fs::path& filename);
+		//void from_instructions_list(const std::vector<std::string>& instructions);
+
+	public:
 		void add_event(const std::shared_ptr<Event>& event);
+		// from "key press a" for example
+		void add_event(const std::string& event_text);
 		/*void to_file(const fs::path&);
 		* void from_file(const fs::path&);
 		*/
--- src/routine/routine.cpp
+++ src/routine/routine.cpp
@@ -1,5 +1,18 @@
 #include "core/core.hpp"
 #include "routine/routine.hpp"
+
+#include "events/mouse/mouse_move.hpp"
+#include "events/mouse/mouse_press.hpp"
+#include "events/mouse/mouse_release.hpp"
+#include "events/mouse/mouse_click.hpp"
+#include "events/mouse/mouse_scroll.hpp"
+
+#include "events/key/key_press.hpp"
+#include "events/key/key_release.hpp"
+#include "events/key/key_click.hpp"
+
+#include "events/time/wait.hpp"
+
 using namespace Virtware;
 
 Routine::Routine()
@@ -9,6 +22,38 @@ Routine::Routine()
 Routine::~Routine()
 {
 	m_events.clear();
+}
+
+void Routine::add_event(const std::string& event_text)
+{
+#define	DETECT_EVENT_STR1(prefix, event_klass) if (event_text.starts_with(prefix)) { \
+			event = std::make_shared<event_klass>(event_text); \
+		}
+#define DETECT_EVENT_STR(prefix, event_klass) else DETECT_EVENT_STR1(prefix, event_klass)
+
+
+	std::shared_ptr<Event> event{ nullptr };
+	DETECT_EVENT_STR1("key press", KeyPressEvent)
+	DETECT_EVENT_STR("key release", KeyReleaseEvent)
+	DETECT_EVENT_STR("key click", KeyClickEvent)
+
+	DETECT_EVENT_STR("mouse move", MouseMoveEvent)
+	//DETECT_EVENT_STR(str, "mouse press", MousePressEvent)
+	//DETECT_EVENT_STR(str, "mouse release", MouseReleaseEvent)
+	//DETECT_EVENT_STR(str, "mouse scroll", MouseScrollEvent)
+
+	DETECT_EVENT_STR("wait", WaitEvent)
+	else
+	{
+		throw std::logic_error("Could not detect event from '" + event_text + '\'');
+	}
+	//TODO: handle other events
+
+
+	this->add_event(event);
+
+#undef DETECT_EVENT_STR1
+#undef DETECT_EVENT_STR
 }
 
 void Routine::add_event(const std::shared_ptr<Event>& event)
