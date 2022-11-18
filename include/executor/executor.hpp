294bd09692934686fc6337e388e8718b7e781707
Bader Ouaich <ouaichbadereddine@gmail.com>
2022-11-18 22:31:48 +0100

    now executor is executing events in a separate thread also we can set a stop keycode, so when the key is pressed the execution will stop

--- include/executor/executor.hpp
+++ include/executor/executor.hpp
@@ -2,23 +2,41 @@
 
 #include "events/event.hpp"
 #include "routine/routine.hpp"
-#include <vector>
 #include <memory>
+#include <queue>
 
 namespace Virtware
 {
 	/**
-	* @brief Executes virtual hardware events
+	* @brief Executes a queue of virtual hardware events in a parallel thread
 	*/
-	class  Executor
+	class Executor
 	{
 	public:
 		Executor();
 		~Executor();
 
 	public:
+		void set_stop_keycode(const std::uint16_t keycode) noexcept;
+		std::uint16_t get_stop_keycode() const noexcept;
+
+		void start();
+		void stop();
 		void execute(const std::shared_ptr<Event>& event);
 		void execute(const std::shared_ptr<Routine>& routine);
-	};
 
+	public:
+		const std::atomic<bool>& is_running() const noexcept;
+
+	private:
+		void executing_loop();
+		void do_event(const std::shared_ptr<Event>& event); // the actual execution of the event
+
+	private:
+		std::queue<std::shared_ptr<Event>> m_events_queue; /*< Queue of events to execute in a parallel thread */
+		std::atomic<bool> m_is_executing;
+		std::unique_ptr<std::thread> m_executing_thread;
+
+		std::uint16_t m_stop_keycode; /*< Escape key by default, If this key was clicked during the execution, it will stop */
+	};
 }
--- src/executor/executor.cpp
+++ src/executor/executor.cpp
@@ -14,20 +14,98 @@
 #include <stdexcept>
 using namespace Virtware;
 
-Executor::Executor()
+Executor::Executor() : 
+	m_stop_keycode(VK_ESCAPE),
+	m_is_executing(false)
 {
+
 }
 
 Executor::~Executor()
 {
+	this->stop();
+}
+
+void Executor::set_stop_keycode(const std::uint16_t keycode) noexcept
+{
+	m_stop_keycode = keycode;
+}
+
+std::uint16_t Executor::get_stop_keycode() const noexcept
+{
+	return this->m_stop_keycode;
+}
+
+void Executor::start()
+{
+	stop();
+	m_executing_thread.reset(new std::thread(&Executor::executing_loop, this));
+	m_is_executing = true;
+}
+
+void Executor::stop()
+{
+	if (not m_is_executing) return;
+
+	m_is_executing = false;
+	if (m_executing_thread && m_executing_thread->joinable())
+	{
+		m_executing_thread->join();
+	}
+
+	// clear queue of events
+	while (not m_events_queue.empty()) 
+	{
+		m_events_queue.pop();
+	}
 }
 
 void Executor::execute(const std::shared_ptr<Event>& event)
 {
+	m_events_queue.push(event);
+}
+
+void Executor::execute(const std::shared_ptr<Routine>& routine)
+{
+	for (const std::shared_ptr<Event>& event : routine->get_events())
+	{
+		m_events_queue.push(event);
+	}
+}
+
+const std::atomic<bool>& Executor::is_running() const noexcept
+{
+	return m_is_executing;
+}
+
+void Executor::executing_loop() 
+{
+	while (m_is_executing)
+	{
+		// Check first if we need to stop execution, if the key set as a stopping flag is pressed
+		if (Key::is_pressed(m_stop_keycode)) 
+		{
+			//TODO: problem how can we stop the execution loop within the loop thread without crashing the thread
+			break;
+		}
+
+		if(not m_events_queue.empty())
+		{ 
+			const std::shared_ptr<Event>& event = m_events_queue.front();
+			do_event(event);
+			m_events_queue.pop();
+		}
+		else // no events to execute, let's chill a bit.
+			std::this_thread::yield();
+	}
+}
+
+void Executor::do_event(const std::shared_ptr<Event>& event)
+{
 	using enum Event::EventType;
 	switch (event->get_type())
 	{
-	case Key: 
+	case Key:
 	{
 		if (KeyPressEvent* key_press_event = dynamic_cast<KeyPressEvent*>(event.get()))
 		{
@@ -49,7 +127,7 @@ void Executor::execute(const std::shared_ptr<Event>& event)
 	}
 	case Mouse:
 	{
-		if(MouseMoveEvent* mouse_move_event = dynamic_cast<MouseMoveEvent*>(event.get()))
+		if (MouseMoveEvent* mouse_move_event = dynamic_cast<MouseMoveEvent*>(event.get()))
 		{
 			Mouse::set_position(mouse_move_event->get_position());
 		}
@@ -68,9 +146,9 @@ void Executor::execute(const std::shared_ptr<Event>& event)
 		}
 		else if (MouseScrollEvent* mouse_scroll_event = dynamic_cast<MouseScrollEvent*>(event.get()))
 		{
-			if(mouse_scroll_event->is_up())
+			if (mouse_scroll_event->is_up())
 				Mouse::scroll_up();
-			else 
+			else
 				Mouse::scroll_down();
 		}
 		else
@@ -93,16 +171,9 @@ void Executor::execute(const std::shared_ptr<Event>& event)
 		}
 		break;
 	}
-	default: 
+	default:
 		throw std::out_of_range("Unhandled Event Type '" + event->to_string() + '\'');
-		
+
 	}
 }
 
-void Executor::execute(const std::shared_ptr<Routine>& routine)
-{
-	for (const std::shared_ptr<Event>& event : routine->get_events())
-	{
-		this->execute(event);
-	}
-}
