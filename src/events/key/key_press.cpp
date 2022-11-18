f1007fd4c5d1be953bb0f6b39d2b75ee6782b594
Bader Ouaich <ouaichbadereddine@gmail.com>
2022-11-18 22:30:22 +0100

    changed parsing method for KeyPressEvent from sscanf to regex

--- src/events/key/key_press.cpp
+++ src/events/key/key_press.cpp
@@ -37,11 +37,22 @@ std::string KeyPressEvent::to_string() const
 
 void KeyPressEvent::from_string(const std::string &str)
 { 
+#if 0
     // Prepare buffer to hold key string
     char keyname[32]{ '\000' };
     // Scan string format to be parsed
-    if (std::sscanf(str.c_str(), "key press %32s", keyname) != 1)
+    if (std::sscanf(str.c_str(), "key press %31s", keyname) != 1)
         throw std::logic_error("Failed to parse KeyPressEvent from '" + str + '\'');
+#endif
+    // Parse expected format to extract keyname
+    std::string keyname;
+    const std::regex rgx("key press ([a-z]{1,32})"); // e.g: key press a OR key press escape ...
+    std::smatch matches;
+    if (std::regex_search(str, matches, rgx) && matches.size() == 2) // matches[0] gives the whole rgx
+    {
+        keyname = matches[1].str();
+    }
+    else throw std::logic_error("Failed to parse KeyPressEvent from '" + str + '\'');
 
     // Validate key name
     const auto it = std::find_if(Key::KEYBOARD.begin(), Key::KEYBOARD.end(), [&keyname](const auto& key)
@@ -55,5 +66,5 @@ void KeyPressEvent::from_string(const std::string &str)
         m_keycode = it->first;
     }
     else 
-        throw std::logic_error("Failed to parse KeyPressEvent. Invalid key name '"+ std::string(keyname) + '\'');
+        throw std::logic_error("Failed to parse KeyPressEvent. Invalid key name '"+ keyname + '\'');
 }
