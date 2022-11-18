8f3fab7053f8deef8eae2d92689af26c592cee0d
Bader Ouaich <ouaichbadereddine@gmail.com>
2022-11-18 22:30:07 +0100

    changed parsing method for KeyReleaseEvent from sscanf to regex

--- src/events/key/key_click.cpp
+++ src/events/key/key_click.cpp
@@ -37,24 +37,27 @@ std::string KeyClickEvent::to_string() const
 
 void KeyClickEvent::from_string(const std::string& str)
 {
-    // Prepare buffer to hold key string
-    char keyname[32]{ '\000' };
-    // Scan string format to be parsed
-    if (std::sscanf(str.c_str(), "key click %32s", keyname) != 1)
-        throw std::logic_error("Failed to parse KeyClickEvent from '" + str + '\'');
-
+    // Parse expected format to extract keyname
+    std::string keyname;
+    const std::regex rgx("key click ([a-z]{1,32})"); // e.g: key click a OR key click escape ...
+    std::smatch matches;
+    if (std::regex_search(str, matches, rgx) && matches.size() == 2) // matches[0] gives the whole rgx
+    {
+        keyname = matches[1].str();
+    }
+    else throw std::logic_error("Failed to parse KeyClickEvent from '" + str + '\'');
 
     // Validate key name
     const auto it = std::find_if(Key::KEYBOARD.begin(), Key::KEYBOARD.end(), [&keyname](const auto& key)
-    {
-        const auto& [keycode, valid_keyname] = key;
-        return keyname == valid_keyname;
-    });
+        {
+            const auto& [keycode, valid_keyname] = key;
+    return keyname == valid_keyname;
+        });
     if (it != Key::KEYBOARD.end())
     {
         // Assign keycode with its correspondent value if valid
         m_keycode = it->first;
     }
     else
-        throw std::logic_error("Failed to parse KeyClickEvent. Invalid key name '" + std::string(keyname) + "\'.");
+        throw std::logic_error("Failed to parse KeyClickEvent. Invalid key name '" + keyname + '\'');
 }
